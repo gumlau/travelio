00baa1704d88fc2fef14cbda9099dc1d590de002
GanLiuuuu <119108256+GanLiuuuu@users.noreply.github.com>
2025-01-19 21:47:14 +0800

    feat: add Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { inject } from '@vercel/analytics'
+
+// Initialize Vercel Analytics
+inject()
 
 const app = createApp(App)
 app.use(router)
